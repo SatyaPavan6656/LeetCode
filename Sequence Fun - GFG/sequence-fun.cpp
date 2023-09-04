7837cf81dfc2c580418785f1aacd7b484f048eb5
Satya Pavan <102037431+SatyaPavan6656@users.noreply.github.com>
2023-09-04 21:30:57 +0530

    Added solution - LeetHub

---
+++ Sequence Fun - GFG/sequence-fun.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+	public:
+		int NthTerm(int n){
+		    // Code  here
+		    long res=1;
+		    for(int i=1;i<=n;i++){
+		        res=((res*i+1)%(1000000007));
+		    }
+		    return (int)res;
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution obj;
+		int ans = obj.NthTerm(n);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends

d136a160a26221980afa6809165cbe8fa3aa9062
Satya Pavan <102037431+SatyaPavan6656@users.noreply.github.com>
2023-10-06 06:57:15 +0530

    Added solution - LeetHub

---
+++ Reversing the vowels - GFG/reversing-the-vowels.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        string modify (string s)
+        {
+            //code here.
+            stack<int> str;
+            string temp="";
+            for(int i=0;i<s.length();i++)
+            {
+                if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+                {
+                    str.push(s[i]);
+                }
+            }
+            for(int i=0;i<s.length();i++)
+            {
+                if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+                {
+                    s[i]=str.top();
+                    str.pop();
+                }
+            }
+            return s;
+
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
